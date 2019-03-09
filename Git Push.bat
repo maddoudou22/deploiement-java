056f6fba46693c72ec677b3ebfe796eb4d9291eb
user <you@example.com>
2019-03-09 10:41:03 +0100

    latest push from windows

---
+++ Git Push.bat
@@ -0,0 +1,18 @@
+
+echo off
+
+SET nom_Projet_Git=deploiement-java
+SET URL_Projet_Git=https://github.com/maddoudou22/deploiement-java.git
+SET Repertoire_Local_Projet=C:\awsLambda\java\deploiement-java
+
+cd %Repertoire_Local_Projet%
+
+echo "Git Add ..."
+git add -A
+
+echo "Git Commit ..."
+git commit -m "latest push from windows"
+
+echo "Git Push ..."
+git push %URL_Projet_Git%
+
